652ab09499a456cb7ba9a92517937ab432fd69bb
mikecoderlz <mikeo.lz17@gmail.com>
2024-07-08 16:55:46 -0400

    Corrected types of dummy initializations in math test.

--- test/math/test.cpp
+++ test/math/test.cpp
@@ -6,8 +6,8 @@ int main( int argc, const char** argv )
 {
     geni::math::Scalar scalar;
     geni::math::Vec2 vec2;
-    geni::math::Vec2 vec3;
-    geni::math::Vec2 vec4;
+    geni::math::Vec3 vec3;
+    geni::math::Vec4 vec4;
 
     return 0;
 }
