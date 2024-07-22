22f1386294bed8f89a1b1b0fa2a32dc3bd7ee7fc
mikecoderlz <mikeo.lz17@gmail.com>
2024-07-22 10:54:53 -0400

    Added std::ostream printing to vectors.

--- source/math/math.hpp
+++ source/math/math.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
 #include "math/vector.hpp"
+#include <iostream>
+
+namespace geni::math {
+
+    inline
+    auto operator <<( std::ostream& s, Vec2 const& v ) -> std::ostream& {
+        s << "<" << v[0] << ", " << v[1] << ">";
+        return s;
+    }
+
+    inline
+    auto operator <<( std::ostream& s, Vec3 const& m ) -> std::ostream& {
+        s << "<" << v[0] << ", " << v[1] << ", " << v[2] << ">";
+        return s;
+    }
+
+    inline
+    auto operator <<( std::ostream& s, Vec4 const& m ) -> std::ostream& {
+        s << "<" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << ">";
+        return s;
+    }
+}
