7c72d7edb35f0c670bce283c01a4b7b62f9e9e8f
mikecoderlz <mikeo.lz17@gmail.com>
2024-08-17 21:13:28 -0400

    Added some scalar trig functions, and DblScalar.

--- source/math/scalar.hpp
+++ source/math/scalar.hpp
@@ -5,6 +5,7 @@
 namespace geni::math {
 
     using Scalar = float;
+    using DblScalar = double;
 
     inline
     Scalar invSqrt( Scalar const a )
@@ -13,5 +14,21 @@ namespace geni::math {
     inline
     Scalar sqrt( Scalar const a )
     { return std::sqrt(a); }
+
+    inline
+    DblScalar invSqrt( DblScalar const a )
+    { return 1.0 / std::sqrt(a); }
+
+    inline
+    DblScalar sqrt( DblScalar const a )
+    { return std::sqrt(a); }
+
+    inline
+    DblScalar sin( DblScalar const a )
+    { return std::sin(a); }
+
+    inline
+    DblScalar cos( DblScalar const a )
+    { return std::cos(a); }
 
 }
